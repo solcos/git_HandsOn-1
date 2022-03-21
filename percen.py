2bbe7cdbd6d1e32145d6ef0e9f01f0e30b908036
root <root@Lenovo-PC.localdomain>
2022-03-21 22:13:02 +0100

    user-defined message

---
+++ percen.py
@@ -0,0 +1,25 @@
+import sys, re
+from argparse import ArgumentParser 
+
+parser = ArgumentParser(description = 'computes the percentage of each nucleotide from a DNA or RNA')
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence") 
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+    
+args = parser.parse_args()
+args.seq = args.seq.upper()  
+a= args.seq.count("U") #this counts the number of U inside the sequence
+b= args.seq.count("A") #this counts the number of A inside the sequence
+c= args.seq.count("C") #this counts the number of C inside the sequence
+d= args.seq.count("T") #this counts the number of T inside the sequence
+e= args.seq.count("G") #this counts the number of G inside the sequence
+
+#the percentages of each ones. 
+print ("Percentage of U's in sequence:", (a / len(args.seq)) * 100) 
+print ("Percentage of A's in sequence:", (b / len(args.seq)) * 100)
+print ("Percentage of C's in sequence:", (c / len(args.seq)) * 100)
+print ("Percentage of T's in sequence:", (d / len(args.seq)) * 100)
+print ("Percentage of G's in sequence:", (e / len(args.seq)) * 100)
+
