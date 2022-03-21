9a079619a823799bbd1113a10f0ba60da85d9515
root <root@Lenovo-PC.localdomain>
2022-03-21 13:05:14 +0100

    user-defined message

--- seqClass.py
+++ seqClass.py
@@ -26,6 +26,6 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("I FOUND a motif")
+        print("I FOUND a motif motif branch")
     else:
         print("NOT FOUND any motif ")
